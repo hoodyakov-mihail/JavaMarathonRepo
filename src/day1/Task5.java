94003fadcdab8f4fbee8709e2c695303ae535c62
Mikhail Khudyakov <hoodyakov.mihail@gmail.com>
2020-08-24 17:26:53 +0700

    complete day1 tasks

---
+++ src/day1/Task5.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for(int age = 30; age <= 100; age += 5) {
+            System.out.println("Мой возраст " + age);
+        }
+    }
+}
---
+++ src/day1/Task6.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 2;
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i + " * " + k + " = " + i * k);
+            i++;
+        }
+    }
+}
